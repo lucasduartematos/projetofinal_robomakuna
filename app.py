fa04b2f4bb904fcc78d12d05b19b9951401a38cc
Lucas Duarte <114267308+lucasduartematos@users.noreply.github.com>
2023-04-10 21:32:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
-
+import gspread
 import requests
 import pandas as pd
 import telegram
-from flask import Flask, request, render_template, Response
+from flask import Flask, request, Response
+from oauth2client.service_account import ServiceAccountCredentials
+from tchan import ChannelScraper
 from bs4 import BeautifulSoup
-
 
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
-bot = telegram.Bot(token=TELEGRAM_API_KEY)
+GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+
+with open("credenciais.json", mode="w") as arquivo:
+    arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+api = gspread.authorize(conta)
+planilha = api.open_by_key("194kfy5ezKLuREJV7UO5mlEkZSYbUMaUQC2q5hi-XKb4")
+sheet = planilha.worksheet("robo_lucasduarte_bot")
 app = Flask(__name__)
 
 menu = """
@@ -19,34 +28,36 @@ menu = """
 
 @app.route("/")
 def index():
-  return menu + "Olá, seja bem-vindo(a) ao site de notícias indígenas do jornalista Lucas Duarte"
+    return menu + "Olá, seja bem-vindo(a) ao site de notícias indígenas do jornalista Lucas Duarte"
 
 @app.route("/sobre")
 def sobre():
-  return menu + "Robô de notícias indígenas: A análise de dados é uma ferramenta poderosa para a compreensão de tendências e padrões em diversos setores da sociedade. No contexto dos povos indígenas, a análise de notícias diárias pode ajudar a identificar padrões de violação de direitos humanos e a mapear a presença dessas comunidades em diferentes regiões. Nesse sentido, a utilização do método de raspagem em Python pode ser uma ferramenta valiosa para coletar e analisar notícias diárias sobre indígenas."
+    return menu + "Robô de notícias indígenas: A análise de dados é uma ferramenta poderosa para a compreensão de tendências e padrões em diversos setores da sociedade. No contexto dos povos indígenas, a análise de notícias diárias pode ajudar a identificar padrões de violação de direitos humanos e a mapear a presença dessas comunidades em diferentes regiões. Nesse sentido, a utilização do método de raspagem em Python pode ser uma ferramenta valiosa para coletar e analisar notícias diárias sobre indígenas."
 
 @app.route("/contato")
 def contato():
-  return menu + "Mais informações: lucasduartematos@gmail.com"
+    return menu + "Mais informações: lucasduartematos@gmail.com"
 
 @app.route("/noticias")
 def noticias_indigenas():
-    requisicao=requests.get('https://site-projeto-robomakuna.onrender.com/noticias')
+    requisicao=requests.get('https://www.cnnbrasil.com.br/tudo-sobre/indigenas/')
     html=BeautifulSoup(requisicao.content)
-    manchetes_indigenas=html.findAll('h2',{'class':'titulo'})
+    manchetes_indigenas=html.findAll('li',{'class':'home__list__item'})
     lista_noticias=[]
     for noticia in manchetes_indigenas:
-        manchete=noticia.text.strip()
-        link=noticia.find('a').get('href')
+        manchete=noticia.text
+        link=noticia.find('a').get('href') 
         lista_noticias.append([manchete, link])
     df=pd.DataFrame(lista_noticias, columns=['Manchete','Link'])
-    df_top5 = df[:5]
-    noticias = []
-    for index, row in df_top5.iterrows():
-        noticias.append(f"{index+1}. {row['Manchete']} - {row['Link']}")
-    mensagem = "\n".join(noticias)
-    bot.send_message(chat_id=TELEGRAM_ADMIN_ID, text=mensagem)
-    return "Notícias enviadas para o chat no Telegram!"
+    return df.tail(5)
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/noticias", methods=["POST"])
+def telegram_bot():
+    update = request.json
+    chat_id = update["message"]["chat"]["id"]
+    message = update["message"]["text"]
+
+    if message == "/start":
+        # Raspa as últimas 5 notícias do site da CNN
+        ultimas_noticias = noticias_indigenas()
+        tabela_html = ultimas_noticias.to
